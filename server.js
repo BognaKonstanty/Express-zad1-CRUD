c1bdbef52ed60898175649b08c5aab29f1264cce
Bogna Konstanty <rosiakb@gmail.com>
2017-07-20 10:25:28 +0200

    correction

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
 var express = require('express');
 var fs = require('fs');
 var app= express();
-var bodyParser = require('body-parser'),
+var bodyParser = require('body-parser');
 var fileContent;
 
 app.use(bodyParser.json());
 
 
 app.get('/getNote', function(req,res) {
-	console.log();
-	fs.readFile('.test.json', 'utf8', function(err, data) {
+	fs.readFile('./test.json', 'utf8', function(err, data) {
 	if (err) throw err;
 	fileContent = data;
 	res.send(data);
 	});
 
-	res.send('zawartość json' + fileContent );
 });
 
 
 app.post('/updateNote/:note', function(req, res) {
-	fileContent = req.params.note;
-	console.log();
-	fs.writeFile('/test.json', stringifyFile, function(err, data) {
+
+	var tmpFileContent = JSON.parse(fileContent);
+	tmpFileContent.push({content: req.params.note});
+	fileContent = JSON.stringify(tmpFileContent, null, 2)
+
+	fs.writeFile('./test.json', fileContent, function(err, data) {
 	if (err) throw err;
 	console.log('file update');
 	});
 
-	res.send(' zawratość json + note' + req.params.note );
+	res.send(req.params.note);
 });
 
 
